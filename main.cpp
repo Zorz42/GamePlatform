d0e132db56dfe2fe75c15a823e278d629066a117
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2020-10-21 08:43:45 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include "swl.h"
+
+void main();
+void main_event(SDL_Event& event);
+Swl::scene main_scene(&main, &main_event);
+
+Swl::texture text;
+Swl::rect_c test_rect;
+
+void preInit() {
+    //swl.window_width = 640;
+    //swl.window_height = 400;
+    //swl.window_caption = "Untitled";
+    //swl.load_font = false;
+    //swl.font_size = 32;
+    
+    // this library already comes with arial.ttf
+    //swl.font_path = "/path/to/font.ttf";
+    swl.goFullscreen();
+}
+
+void postInit() {
+    swl.switchScene(main_scene);
+    std::cout << swl.window_width << std::endl;
+    test_rect.w = 100;
+    test_rect.h = 100;
+    test_rect.x = swl.window_width - test_rect.w;
+    test_rect.y = swl.window_height - test_rect.h;
+    test_rect.c = {255, 255, 255};
+}
+
+void main_event(SDL_Event& event) {
+    if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)
+        swl.stop();
+}
+
+void main() {
+    swl.draw(test_rect);
+    /* this is the main loop which executes every frame */
+}
