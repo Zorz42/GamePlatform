11040761a123dd50546a5c966e2204c12bcbba5e
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2020-10-27 14:34:33 +0100

    added clock

--- MainScreen/mainScreen.cpp
+++ MainScreen/mainScreen.cpp
@@ -4,6 +4,11 @@
 #include "tiles.h"
 #include "topBar.h"
 #include "selectionRect.h"
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+
+#define TIME_SPACING_RIGHT 25
 
 void mainScreen::init() {
     main_screen_scene.background_color = {10, 10, 10};
@@ -25,4 +30,13 @@ void mainScreen::main_render() {
     selectionRect::render();
     tiles::render();
     topBar::render();
+    
+    static Swl::texture time_texture;
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+    std::stringstream time_string;
+    time_string << std::put_time(&tm, "%H:%M");
+    time_texture.loadFromText(time_string.str(), {255, 255, 255});
+    time_texture.x = swl.window_width - time_texture.getWidth() - TIME_SPACING_RIGHT;
+    swl.draw(time_texture);
 }
